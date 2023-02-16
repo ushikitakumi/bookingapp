580e81cdac28592ca8f9ddb14e692d85dd08aae3
ushikitakumi <cowki.1999@gmail.com>
2023-02-16 13:20:27 +0900

    MySQLに変更

--- bookingapp/settings.py
+++ bookingapp/settings.py
@@ -85,11 +85,14 @@ WSGI_APPLICATION = 'bookingapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bookingapp',
+        'USER': 'root',
+        'PASSWORD': 'Gnubass1999',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
